e66eabc53db0cdd335de9d84599360fab386d5f1
William Kariampuzha <86256217+wzkariampuzha@users.noreply.github.com>
2021-09-21 15:32:38 -0500

    Add files via upload

--- classify_abs.py
+++ classify_abs.py
@@ -3,9 +3,8 @@ import requests
 import xml.etree.ElementTree as ET
 import pickle
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 from nltk.corpus import stopwords
 import spacy
 import numpy as np
@@ -46,35 +45,172 @@ def standardizeSciTerms(abstract, nlpSci, nlpSci2):
         newAbstract = newAbstract[:start] + e.label_ + newAbstract[end:]
     return newAbstract
 
+# Prepare model
+#nlp, nlpSci, nlpSci2, classify_model, classify_tokenizer= init_classify_model()
+def init_classify_model(model='my_model_orphanet_final'):
+    #Load spaCy models
+    nlp = spacy.load('en_core_web_lg')
+    nlpSci = spacy.load("en_ner_bc5cdr_md")
+    nlpSci2 = spacy.load('en_ner_bionlp13cg_md')
+    
+    # load the tokenizer
+    with open('tokenizer.pickle', 'rb') as handle:
+        classify_tokenizer = pickle.load(handle)
+    
+    # load the model
+    classify_model = tf.keras.models.load_model(model) 
+    
+    return nlp, nlpSci, nlpSci2, classify_model, classify_tokenizer
+
+#Gets abstract and title (concatenated) from EBI API
+def PMID_getAb(PMID): 
+    url = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=EXT_ID:'+str(PMID)+'&resulttype=core'
+    r = requests.get(url)
+    root = ET.fromstring(r.content)
+    titles = [title.text for title in root.iter('title')]
+    abstracts = [abstract.text for abstract in root.iter('abstractText')]
+    if len(abstracts) > 0 and len(abstracts[0])>5:
+        return titles[0]+' '+abstracts[0]
+    else:
+        return ''
+
+## DEPRECATED, use search_getAbs for more comprehensive results
+def search_Pubmed_API(searchterm, maxResults): #returns a dictionary of {pmids:abstracts} 
+    # get results from searching for disease name through PubMed API
+    term = ''
+    dz_words = searchterm.split()
+    for word in dz_words:
+        term += word + '%20'
+    query = term[:-3]
+    url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term='+query
+    r = requests.get(url)
+    root = ET.fromstring(r.content)
+
+    pmids = []
+    i = 0
+
+    # loop over resulting articles
+    for result in root.iter('IdList'):
+        pmids = [pmid.text for pmid in result.iter('Id')]
+        if i >= maxResults:
+            break
+        pmid_to_abs = {}
+        for pmid in pmids:
+            abstract = PMID_getAb(pmid)
+            if len(abstract)>5:
+                pmid_to_abs[pmid]=abstract
+                i+=1
+    return pmid_to_abs
+
+## DEPRECATED, use search_getAbs for more comprehensive results
+# get results from searching for disease name through EBI API
+def search_EBI_API(search_term,maxResults): #returns a dictionary of {pmids:abstracts}    
+    term = ''
+    dz_words = search_term.split()
+    for word in dz_words:
+        term += word + '%20'
+    query = term[:-3]
+    url = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query='+query+'&resulttype=core'
+    r = requests.get(url)
+    root = ET.fromstring(r.content)
+
+    pmids_abs = {}
+    i = 0
+
+    # loop over resulting articles
+    for result in root.iter('result'):
+        if i >= maxResults:
+            break
+        pmids = [pmid.text for pmid in result.iter('id')]
+        if len(pmids) > 0:
+            pmid = pmids[0]
+            if pmid[0].isdigit():
+                abstracts = [abstract.text for abstract in result.iter('abstractText')]
+                titles = [title.text for title in result.iter('title')]
+                if len(abstracts) > 0:# and len(abstracts[0])>5:
+                    pmids_abs[pmid] = titles[0]+' '+abstracts[0]
+                    i+=1
+     
+    return pmids_abs
+
+## This is the main, most comprehensive search_term function, it can take in a search term or a list of search terms and output a dictionary of {pmids:abstracts}
+## Gets results from searching through both PubMed and EBI search term APIs, also makes use of the EBI API for PMIDs. 
+## EBI API and PubMed API give different results
+# This makes n+2 API calls where n<=maxResults, which is slow 
+# There is a way to optimize by gathering abstracts from the EBI API when also getting pmids but did not pursue due to time constraints
+def search_getAbs(searchterm_list, maxResults):
+    #Keep track of total results
+    i = 0
+    
+    #set of all pmids
+    pmids = set()
+    
+    #dictionary {pmid:abstract}
+    pmid_abs = {}
+    
+    #type validation, allows string or list input
+    if type(searchterm_list)!=list:
+        if type(searchterm_list)==str:
+            searchterm_list = [searchterm_list]
+        else:
+            searchterm_list = list(searchterm_list)
+    
+    #gathers pmids into a set first
+    for dz in searchterm_list:
+        term = ''
+        dz_words = dz.split()
+        for word in dz_words:
+            term += word + '%20'
+        query = term[:-3]
+
+        ## get pmid results from searching for disease name through PubMed API
+        url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term='+query
+        r = requests.get(url)
+        root = ET.fromstring(r.content)
+
+        # loop over resulting articles
+        for result in root.iter('IdList'):
+            if i >= maxResults:
+                break
+            pmidlist = [pmid.text for pmid in result.iter('Id')]
+            pmids.update(pmidlist)
+            i+=len(pmidlist)
+
+        ## get results from searching for disease name through EBI API
+        url = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query='+query+'&resulttype=core'
+        r = requests.get(url)
+        root = ET.fromstring(r.content)
+
+        # loop over resulting articles
+        for result in root.iter('result'):
+            if i >= maxResults:
+                break
+            pmidlist = [pmid.text for pmid in result.iter('id')]
+            #can also gather abstract and title here but for some reason did not work as intended the first time
+            if len(pmidlist) > 0:
+                pmid = pmidlist[0]
+                if pmid[0].isdigit():
+                    pmids.add(pmid)
+                    i += 1
+
+    ## get abstracts from EBI PMID API and output a dictionary
+    for pmid in pmids:
+        abstract = PMID_getAb(pmid)
+        if len(abstract)>5:
+            pmid_abs[pmid] = abstract
+    
+    return pmid_abs
+
 # Generate predictions for a PubMed Id
 # nlp: en_core_web_lg
 # nlpSci: en_ner_bc5cdr_md
 # nlpSci2: en_ner_bionlp13cg_md
 # Defaults to load my_model_orphanet_final, the most up-to-date version of the classification model,
 # but can also be run on any other tf.keras model
-
-
-def get_abstract(pmid):
-# retrieve abstract from EBI API
-    url = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=EXT_ID:'+str(pmid)+'&resulttype=core'
-    r = requests.get(url)
-    root = ET.fromstring(r.content)
+#This was originally getPredictions
+def getPMIDPredictions(pmid, nlp, nlpSci, nlpSci2, classify_model, classify_tokenizer):
     
-    abstract = ''
-    for child in root.iter('*'):
-        if child.tag == 'abstractText':
-            abstract = child.text
-    return abstract
-
-
-def getPredictions(pmid, nlp, nlpSci, nlpSci2, model='my_model_orphanet_final'):
-    abstract = get_abstract(pmid)
-    
-    # load the tokenizer
-    with open('tokenizer.pickle', 'rb') as handle:
-        new_tokenizer = pickle.load(handle)
-    
-    new_model = tf.keras.models.load_model('saved_model/'+model) # load the model
+    abstract = PMID_getAb(pmid)
     
     # remove stopwords
     for word in STOPWORDS:
@@ -84,10 +220,10 @@ def getPredictions(pmid, nlp, nlpSci, nlpSci2, model='my_model_orphanet_final'):
         
     # preprocess abstract
     abstract_standard = [standardizeAbstract(standardizeSciTerms(abstract, nlpSci, nlpSci2), nlp)]
-    sequence = new_tokenizer.texts_to_sequences(abstract_standard)
+    sequence = classify_tokenizer.texts_to_sequences(abstract_standard)
     padded = pad_sequences(sequence, maxlen=max_length, padding=padding_type, truncating=trunc_type)
     
-    y_pred1 = new_model.predict(padded) # generate prediction
+    y_pred1 = classify_model.predict(padded) # generate prediction
     y_pred = np.argmax(y_pred1, axis=1) # get binary prediction
     
     prob = y_pred1[0][1]
@@ -96,19 +232,10 @@ def getPredictions(pmid, nlp, nlpSci, nlpSci2, model='my_model_orphanet_final'):
     else:
         isEpi = False
 
-    return prob, isEpi
-   
+    return abstract, prob, isEpi
 
-def getAbstractPredictions(abstract, nlp, nlpSci, nlpSci2, model='my_model_orphanet_final'):
-    
-    if(len(abstract)<5):
-        abstract = get_abstract(pmid)
-        
-    # load the tokenizer
-    with open('tokenizer.pickle', 'rb') as handle:
-        new_tokenizer = pickle.load(handle)
-    
-    new_model = tf.keras.models.load_model('saved_model/'+model) # load the model
+
+def getTextPredictions(abstract, nlp, nlpSci, nlpSci2, classify_model, classify_tokenizer):
     
     # remove stopwords
     for word in STOPWORDS:
@@ -118,10 +245,10 @@ def getAbstractPredictions(abstract, nlp, nlpSci, nlpSci2, model='my_model_orpha
         
     # preprocess abstract
     abstract_standard = [standardizeAbstract(standardizeSciTerms(abstract, nlpSci, nlpSci2), nlp)]
-    sequence = new_tokenizer.texts_to_sequences(abstract_standard)
+    sequence = classify_tokenizer.texts_to_sequences(abstract_standard)
     padded = pad_sequences(sequence, maxlen=max_length, padding=padding_type, truncating=trunc_type)
     
-    y_pred1 = new_model.predict(padded) # generate prediction
+    y_pred1 = classify_model.predict(padded) # generate prediction
     y_pred = np.argmax(y_pred1, axis=1) # get binary prediction
     
     prob = y_pred1[0][1]
@@ -133,15 +260,11 @@ def getAbstractPredictions(abstract, nlp, nlpSci, nlpSci2, model='my_model_orpha
     return prob, isEpi
 
 if __name__ == '__main__':
-    print('Loading 3 NLP models...')
-    nlp = spacy.load('en_core_web_lg')
-    print('Core model loaded.')
-    nlpSci = spacy.load("en_ner_bc5cdr_md")
-    print('Disease and chemical model loaded.')
-    nlpSci2 = spacy.load('en_ner_bionlp13cg_md')
+    print('Loading 5 NLP models...')
+    nlp, nlpSci, nlpSci2, classify_model, classify_tokenizer= init_classify_model()
     print('All models loaded.')
     pmid = input('\nEnter PubMed PMID (or DONE): ')
     while pmid != 'DONE':
-        prob, isEpi = getPredictions(pmid, nlp, nlpSci, nlpSci2)
-        print(prob, isEpi)
+        abstract, prob, isEpi = getPredictions(pmid, nlp, nlpSci, nlpSci2, classify_model, classify_tokenizer)
+        print(abstract, prob, isEpi)
         pmid = input('\nEnter PubMed PMID (or DONE): ')
